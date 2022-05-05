b513d0b99fcf8b978a52a0b574bf7544b79fdd15
Ruben35 <ruben111416@gmail.com>
2022-05-05 17:23:33 -0500

    Fixing bug when packaging app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import { Signature ,Cipher, MainMenu } from './pages'
 import './styles/main.css'
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainMenu/>} />
         <Route path="/cipher" element={<Cipher/>} />
         <Route path="/signature" element={<Signature/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
